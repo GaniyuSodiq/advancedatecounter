248aeafb5e06206bd8d605c16949721a9619fed5
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-02-02 13:06:35 +0100

    make the range and iput works well

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import { useState } from "react";
 
 function App() {
   const [rangeValue, setRangeValue] = useState(1);
+  const [countValue, setCountValue] = useState(0);
   return (
     <div className="App">
-      <input
-        type="range"
-        max={10}
-        min={1}
-        onChange={(e) => setRangeValue(e.target.value)}
-        // value={rangeValue}
-      />{" "}
-      {rangeValue}
+      <div>
+        <input
+          type="range"
+          max={10}
+          min={1}
+          onChange={(e) => setRangeValue(Number(e.target.value))}
+          value={rangeValue}
+        />{" "}
+        {rangeValue}
+      </div>
+      <div>
+        <button onClick={() => setCountValue(countValue - rangeValue)}>
+          -
+        </button>
+        <input
+          type="text"
+          value={countValue}
+          onChange={(e) => setCountValue(Number(e.target.value))}
+        />
+        <button onClick={() => setCountValue(countValue + rangeValue)}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
